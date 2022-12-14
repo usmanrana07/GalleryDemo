b9572879f8e84f3a86ea092951e18a65f1e376d7
Usman Ishrat <usman.rana@ilsainteractive.com>
2022-12-15 00:12:32 +0500

    gradle: added splash api and hilt dependencies along with some refactoring

--- app/src/main/java/com/gallerydemo/MainActivity.kt
+++ app/src/main/java/com/gallerydemo/MainActivity.kt
@@ -2,13 +2,14 @@ package com.gallerydemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.gallerydemo.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
