a6794a6d0e1ddf80d50dc3d1551662862569a0d7
Usman Ishrat <usman.rana@ilsainteractive.com>
2022-12-14 23:19:30 +0500

    first commit to create repository

---
+++ app/src/main/java/com/gallerydemo/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.gallerydemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gallerydemo.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/gallerydemo/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.gallerydemo.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
