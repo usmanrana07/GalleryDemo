762101dfa1aa04fa02acdf3a28abc606c149a1fb
Usman Ishrat <usman.rana@ilsainteractive.com>
2022-12-16 18:27:52 +0500

    MainActivity.kt: added callback to onBackPressedDispatcher for handling backstack

--- app/src/main/java/com/gallerydemo/ui/main/MainActivity.kt
+++ app/src/main/java/com/gallerydemo/ui/main/MainActivity.kt
@@ -1,17 +1,39 @@
 package com.gallerydemo.ui.main
 
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.gallerydemo.R
+import com.gallerydemo.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-           installSplashScreen()
-        setContentView(R.layout.activity_main)
+        installSplashScreen()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setBackPressCallback()
+    }
+
+    private fun setBackPressCallback() {
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val navController =
+                    Navigation.findNavController(binding.navigationHostFragmentContainer)
+                if (navController.currentDestination?.id == R.id.navigation_media)
+                    navController.popBackStack()
+                else
+                    finish()
+            }
+
+        })
     }
 }
