8f70282903b6901e643e1646e9639f324cfc4954
Usman Ishrat <usman.rana@ilsainteractive.com>
2022-12-16 18:31:12 +0500

    renamed MainActivity and related files to GalleryActivity

--- app/src/main/java/com/gallerydemo/ui/main/GalleryActivity.kt
+++ app/src/main/java/com/gallerydemo/ui/main/GalleryActivity.kt
@@ -7,18 +7,18 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import com.gallerydemo.R
-import com.gallerydemo.databinding.ActivityMainBinding
+import com.gallerydemo.databinding.ActivityGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class GalleryActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityGalleryBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_gallery)
 
         setBackPressCallback()
     }
